baefd728c0e45865b50152642bf2233e3b9e1354
akshayb407 <akshayb407@gmail.com>
2022-08-25 15:09:18 +0530

    UC1_ValidFirst_Name added

--- UserRegex/Program.cs
+++ UserRegex/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegex
 {
-    internal class Program
+    public class Program
     {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome_To_UserRegistration_Regex");
+            FirstNameVal();
         }
+        public static void FirstNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter First Name: ");
+            string first_name = Console.ReadLine();
+            bool validate = rg.IsMatch(first_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + first_name);
+            else
+                Console.WriteLine("Invalid FirstName");
+
+        }
+
+
+       
     }
 }
