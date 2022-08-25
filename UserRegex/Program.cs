91854e5523c47a3fb33ecabc56dd895696195754
akshayb407 <akshayb407@gmail.com>
2022-08-25 15:53:09 +0530

    UC3_ValidEmail added

--- UserRegex/Program.cs
+++ UserRegex/Program.cs
@@ -6,11 +6,15 @@ namespace UserRegex
     public class Program
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
+        public static string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_Regex");
             FirstNameVal();
+            LastNameVal();
+            EmailValidation();
         }
+
         public static void FirstNameVal()
         {
             Regex rg = new Regex(REGEX_PATTERN);
@@ -34,8 +38,16 @@ namespace UserRegex
             else
                 Console.WriteLine("Invalid LastName");
         }
-
-
-
+        public static void EmailValidation()
+        {
+            Regex rg = new Regex(EMAIL_PATTERN);
+            Console.Write("Enter E-mail: ");
+            string email = Console.ReadLine();
+            bool validate = rg.IsMatch(email);
+            if (validate)
+                Console.WriteLine("Email is valid : " + email);
+            else
+                Console.WriteLine("Invalid E-mail.");
+        }
     }
 }
