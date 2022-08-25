af536a6812f3232450bbcc09980d8d6795a50131
akshayb407 <akshayb407@gmail.com>
2022-08-25 16:18:10 +0530

    UC5_Password_Rule_1 added

--- UserRegex/Program.cs
+++ UserRegex/Program.cs
@@ -6,23 +6,15 @@ namespace UserRegex
     public class Program
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
-<<<<<<< HEAD
-        public static string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
-=======
-       
->>>>>>> UC2_ValidLast_Name
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome_To_UserRegistration_Regex");
+            Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
             FirstNameVal();
             LastNameVal();
-<<<<<<< HEAD
             EmailValidation();
-=======
-           
->>>>>>> UC2_ValidLast_Name
+            MobileValidation();
+            PasswordRule();
         }
-
         public static void FirstNameVal()
         {
             Regex rg = new Regex(REGEX_PATTERN);
@@ -33,7 +25,6 @@ namespace UserRegex
                 Console.WriteLine("Name is Valid : " + first_name);
             else
                 Console.WriteLine("Invalid FirstName");
-
         }
         public static void LastNameVal()
         {
@@ -46,9 +37,9 @@ namespace UserRegex
             else
                 Console.WriteLine("Invalid LastName");
         }
-<<<<<<< HEAD
         public static void EmailValidation()
         {
+            string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
             Regex rg = new Regex(EMAIL_PATTERN);
             Console.Write("Enter E-mail: ");
             string email = Console.ReadLine();
@@ -58,9 +49,30 @@ namespace UserRegex
             else
                 Console.WriteLine("Invalid E-mail.");
         }
-=======
-       
+        public static void MobileValidation()
+        {
+            string MobilePattern = "^(91){1}[ ]+[0-9]{10}$";
+            Regex rg = new Regex(MobilePattern);
+            Console.Write("Enter Mobile_NO: ");
+            string mobile = Console.ReadLine();
+            bool validate = rg.IsMatch(mobile);
+            if (validate)
+                Console.WriteLine("Mobile is valid : " + mobile);
+            else
+                Console.WriteLine("Invalid Mobile..");
+        }
+        public static void PasswordRule()
+        {
+            string PasswordPattern = "^[a-zA-Z0-9]{8,}$";
+            Regex rg = new Regex(PasswordPattern);
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+            bool validate = rg.IsMatch(password);
+            if (validate)
+                Console.WriteLine("Password is valid : " + password);
+            else
+                Console.WriteLine("Invalid password..");
+        }
 
->>>>>>> UC2_ValidLast_Name
     }
 }
